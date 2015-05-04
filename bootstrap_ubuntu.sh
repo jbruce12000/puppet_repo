45576a2f209f81de4c7103f060fac4f5cf54c84d
jbruce <jbruce12000@gmail.com>
2015-05-04 08:25:13 -0400

    -check if librarian-puppet already installed

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -6,8 +6,13 @@ set -x
 sudo apt-get install -y puppet ruby ruby-dev wget unzip
 
 # Install Librarian
-sudo gem install --no-ri --no-rdoc librarian-puppet
+if sudo gem list|grep librarian-puppet; then
+  echo "OK skipping gem install librarian-puppet"
+else
+  echo "OK installing librarian-puppet"
+  sudo gem install --no-ri --no-rdoc librarian-puppet
+fi
 
 # Use Librarian to pull down modules and puppet apply
-librarian-puppet install
+sudo librarian-puppet install
 sudo puppet apply --hiera_config hiera.yaml --verbose --debug --modulepath=modules manifests/site.pp
